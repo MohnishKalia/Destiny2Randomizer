361c154474e1136406b99d13538b9360f35f40ad
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-05-13 18:29:06 -0500

    Crucible maps fetched

---
+++ preload/index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import fetch from 'node-fetch';
+import { getDestinyManifest, getDestinyManifestSlice } from 'bungie-api-ts/destiny2';
+import fs from 'fs/promises';
+
+const $http = async (config) => {
+    const res = await fetch(config.url, {
+        headers: {
+            'X-API-Key': process.env.API_KEY,
+        }
+    });
+    return res.json();
+}
+
+async function main() {
+    const destinyManifest = await getDestinyManifest($http);
+    const manifestTables = await getDestinyManifestSlice($http, {
+        destinyManifest: destinyManifest.Response,
+        tableNames: ['DestinyActivityDefinition'],
+        language: 'en'
+    });
+
+    const pvpMaps = Object.entries(manifestTables.DestinyActivityDefinition)
+        .filter(([, v]) => v.isPvP && v.activityModeTypes.includes(5) && !v.isPlaylist && /^\/img\/destiny_content\/pgcr\/(pvp|crucible)/.test(v.pgcrImage))
+        .map(([, v]) => ({
+            name: v.displayProperties.name,
+            url: v.pgcrImage
+        }))
+        .sort((a, b) => {
+            const simpA = a.name.replace('The ', '');
+            const simpB = b.name.replace('The ', '');
+            return simpA.localeCompare(simpB);
+        })
+        ;
+
+    const map = {};
+    pvpMaps.forEach(obj => (map[obj.name] = `https://bungie.net${obj.url}`));
+
+    // console.log(JSON.stringify(pvpMaps, null, 4));
+    await fs.writeFile('./data.js', 'export default ' + JSON.stringify(map, null, 4));
+    console.log(map);
+}
+
+main();
